888f3fa72f46d2fceed1086f352ab6b744c095a1
xiaobaiseven <1146112953@qq.com>
2026-04-30 15:14:19 +0800

    changed font to MiSans and Maple Mono NF

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const unsigned int gappoh =
 static const unsigned int gappov =
     7; /* vert outer gap between windows and screen edge */
 static int smartgaps =
-    0; /* 1 means no outer gap when there is only one window */
+    1; /* 1 means no outer gap when there is only one window */
 static const int showbar = 1;       /* 0 means no bar */
 static const int topbar = 1;        /* 0 means bottom bar */
 #define ICONSIZE 15                 /* icon size */
@@ -29,10 +29,10 @@ static const int systraypinningfailfirst =
     1; /* 1: if pinning fails, display systray on the first monitor, False:
           display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
-static const char *fonts[] = {"monospace:style=Bold:size=10",
-                              "FiraCode Nerd Font Mono:size=12",
-                              "MiSans:size=10"};
-static const char dmenufont[] = "monospace:size=10";
+static const char *fonts[] = {"MiSans:style=Normal:size=10",
+                              "Maple Mono NF:size=12"
+                              };
+static const char dmenufont[] = "MiSans:size=10";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
@@ -68,9 +68,9 @@ static Sp scratchpads[] = {
 
 /* tagging */
 
-static const char *tags[] = {"\uf015¹", "\ue5fe²", "\uf030³",
-                             "\uf268⁴", "\uf269⁵", "\ue62a⁶",
-                             "\ue217⁷", "\uf2dc⁸", "\uf30d⁹"};
+static const char *tags[] = {"\uf015  ¹", "\ue5fe  ²", "\uf030  ³",
+                             "\uf268  ⁴", "\uf269  ⁵", "\ue62a  ⁶",
+                             "\ue217  ⁷", "\uf2dc  ⁸", "\uf30d  ⁹"};
 static const Rule rules[] = {
     /* xprop(1):
      *	WM_CLASS(STRING) = instance, class
@@ -86,7 +86,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.5; /* factor of master area size [0.05..0.95] */
+static const float mfact = 0.62; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;   /* number of clients in master area */
 static const int resizehints =
     1; /* 1 means respect size hints in tiled resizals */
