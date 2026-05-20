37d1eaeb4e0edb5c3a753fd31b85d13bec652ead
xiaobaiseven <1146112953@qq.com>
2026-05-20 16:00:05 +0800

    changed fonts to source font faimly

--- config.h
+++ config.h
@@ -24,15 +24,13 @@ static const Bool viewontag = True; /* Switch view on tag switch */
 static const unsigned int systraypinning =
     0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
           X */
-static const unsigned int systrayspacing = 2; /* systray spacing */
+static const unsigned int systrayspacing = 1; /* systray spacing */
 static const int systraypinningfailfirst =
     1; /* 1: if pinning fails, display systray on the first monitor, False:
           display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
-static const char *fonts[] = {"Noto Sans CJK SC:style=Normal:size=10",
-                              "FiraCode Nerd Font Mono:size=12"
-                              };
-static const char dmenufont[] = "Noto Sans CJK SC:size=10";
+static const char *fonts[] = {"Source Han Sans CN:style=Normal:size=10", "SauceCodePro NF:style=Regular:size=12"};
+static const char dmenufont[] = "Source Han Sans CN:size=10";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
